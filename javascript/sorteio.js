43e24ba24d38dc467066562e945a61b6dadb7e55
Bruno Bastos <brunobastos_1998@hotmail.com>
2021-09-12 13:19:55 -0300

    add componentes

--- javascript/sorteio.js
+++ javascript/sorteio.js
@@ -1,12 +1,35 @@
 function sorteio(){
-
-    document.getElementById("audio-control").play();
+    let file = document.getElementById("file").files[0];
     var NUMEROS = [0,1,2,3,4,5,6,7,8,9];
     let casaAtual = 0;
     let atual = 0;
-    
-    let numFinal = "33164875819";
-    let nome = "Bruno Bastos";
+    let data = [];
+    let numFinal = null;
+    let nome = null;
+    let tempoEscolha = 1000;
+
+    if(document.getElementById("tempoescolha")){
+        tempoEscolha = document.getElementById("tempoescolha").value;
+    }
+    if(file == undefined){
+        alert("Precisa fazer upload de um arquivo com dados");
+        return;
+    }
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.addEventListener('load', function (e) {
+        lines = e.target.result.split("\r\n");
+        let selected =Math.floor(Math.random() * (lines.length -1));
+        a = lines[selected].split(";")
+        numFinal = a[1]
+        nome = a[0]
+        console.log(numFinal + "-"+nome)
+      });
+    document.getElementById("audio-control").play();
+    document.getElementById("btn-pararmusica").hidden = false;
+    document.getElementById("btn-tocarmusica").hidden = false;
+    document.getElementById("vencedor").innerHTML = "";
+
     
     let casa1= setInterval(()=>trocaNumero(document.getElementById("casa-0")), 100);
     let casa2= setInterval(()=>trocaNumero(document.getElementById("casa-1")), 100);
@@ -21,18 +44,19 @@ function sorteio(){
     let casa11= setInterval(()=>trocaNumero(document.getElementById("casa-10")), 100);
     
     let casas = [casa1, casa2, casa3, casa4, casa5, casa6, casa7, casa8, casa9, casa10, casa11];
-    setInterval(trocaCasa, 1000);
+    setInterval(trocaCasa, tempoEscolha);
     
     function trocaCasa(){
         let casa = getCasa();
         clearInterval(casas[casaAtual]);
         casa.innerHTML = numFinal.charAt(casaAtual);
-        casaAtual++;
+        ++casaAtual;
         if(casaAtual == casas.length){
             document.getElementById("vencedor").innerHTML = nome;
         }
     }
-    
+
+
     function trocaNumero(e)
     {
         let casa = e;
@@ -52,4 +76,12 @@ function sorteio(){
             atual++;
         }
     }    
+}
+
+function pararmusica(){
+    document.getElementById("audio-control").pause();
+}
+
+function tocarmusica(){
+    document.getElementById("audio-control").play();
 }
